03b743a1157b5072f3e746811d3ee6d3c044efa4
McKURDi 27 <mckurdi27@gmail.com>
2026-08-26 04:53:22 +0200

    Fix spelling of months in HicriTakvimAnaListe
    Updated variable names for months 3 and 4 to correct spelling.

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 window.HicriTakvimAnaListe = {
     "01": typeof window.muharrem_veri !== 'undefined' ? window.muharrem_veri : null,
     "02": typeof window.sefer_veri !== 'undefined' ? window.sefer_veri : null, // Düzeltildi (sefer)
-    "03": typeof window.rebiulevvel_veri !== 'undefined' ? window.rebiulevvel_veri : null,
-    "04": typeof window.rebiulahir_veri !== 'undefined' ? window.rebiulahir_veri : null,
+    "03": typeof window.rebiülevvel_veri !== 'undefined' ? window.rebiülevvel_veri : null,
+    "04": typeof window.rebiülahir_veri !== 'undefined' ? window.rebiülahir_veri : null,
     "05": typeof window.cemaziyelevvel_veri !== 'undefined' ? window.cemaziyelevvel_veri : null,
     "06": typeof window.cemaziyelahir_veri !== 'undefined' ? window.cemaziyelahir_veri : null,
     "07": typeof window.recep_veri !== 'undefined' ? window.recep_veri : null,
