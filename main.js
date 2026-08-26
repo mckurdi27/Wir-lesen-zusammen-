2b4302b43388d31fac8d69450ba78774eeeff674
McKURDi 27 <mckurdi27@gmail.com>
2026-08-26 03:13:09 +0200

    Revise main.js to handle 12 months of data
    Updated main.js to manage all 12 months' data.

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-// Ana dizindeki main.js dosyası
-// json/ klasöründen yüklenen ay verilerini tek bir merkezde toplar.
-
+// Ana dizindeki main.js dosyası - Tüm 12 ayı yönetir
 window.HicriTakvimAnaListe = {
+    "01": typeof window.muharrem_veri !== 'undefined' ? window.muharrem_veri : null,
+    "02": typeof window.safer_veri !== 'undefined' ? window.safer_veri : null,
+    "03": typeof window.rebiulevvel_veri !== 'undefined' ? window.rebiulevvel_veri : null,
+    "04": typeof window.rebiulahir_veri !== 'undefined' ? window.rebiulahir_veri : null,
+    "05": typeof window.cemaziyelevvel_veri !== 'undefined' ? window.cemaziyelevvel_veri : null,
+    "06": typeof window.cemaziyelahir_veri !== 'undefined' ? window.cemaziyelahir_veri : null,
+    "07": typeof window.recep_veri !== 'undefined' ? window.recep_veri : null,
+    "08": typeof window.saban_veri !== 'undefined' ? window.saban_veri : null,
+    "09": typeof window.ramazan_veri !== 'undefined' ? window.ramazan_veri : null,
     "10": typeof window.sevval_veri !== 'undefined' ? window.sevval_veri : null,
     "11": typeof window.zilkade_veri !== 'undefined' ? window.zilkade_veri : null,
     "12": typeof window.zilhicce_veri !== 'undefined' ? window.zilhicce_veri : null
