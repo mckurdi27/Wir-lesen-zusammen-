0fa1a011b3823a298a155ff81403395e6c675c1e
McKURDi 27 <mckurdi27@gmail.com>
2026-08-26 05:29:37 +0200

    Create A.js with initial content structure
    Initial export of religious texts and quotes in A.js.

---
+++ pdf/A.js
@@ -0,0 +1,10 @@
+export default {
+    1: { 
+        ayet: { meal: "Ayet meali buraya gelecek...", kaynak: "Bakara Suresi, 1-3" }, 
+        hadis: { meal: "Hadis meali buraya gelecek...", kaynak: "Buhari" }, 
+        atasozu: { text: "Atasözü içeriği buraya gelecek...", kaynak: "Türk Atasözü" },
+        ozlu_soz: { text: "Özlü söz içeriği buraya gelecek...", kaynak: "Düşünür Adı" },
+        german: { quote: "Beispiel auf Deutsch...", translation: "Almanca çevirisi..." } 
+    },
+    2: { ... }
+};
